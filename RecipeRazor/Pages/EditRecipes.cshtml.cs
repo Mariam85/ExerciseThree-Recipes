f5cd7261c70fda68f195c496ec19494a56a873dd
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-07-22 02:02:58 +0200

    Added more razor pages.

---
+++ RecipeRazor/Pages/EditRecipes.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipeRazor.Pages
+{
+    public class EditRecipesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
