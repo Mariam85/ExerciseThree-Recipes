65fb5ca757bcf8d1e194c60e0ebeabb48cb68562
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-07-28 00:51:17 +0200

    Add handler methods.

--- RecipeRazor/Pages/EditRecipes.cshtml.cs
+++ RecipeRazor/Pages/EditRecipes.cshtml.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeRazor.Models;
 
 namespace RecipeRazor.Pages
 {
     public class EditRecipesModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public EditRecipesModel(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
 
+        [BindProperty]
+        public List<Recipe> recipesList { get; set; } = new List<Recipe>();
+
+        public async Task OnGet()
+        {
+            var client = _httpClientFactory.CreateClient("Recipes");
+            recipesList = await client.GetFromJsonAsync<List<Recipe>>("recipes");
+        }
+        public async Task OnPostUpdate(string info)
+        {
+            var client = _httpClientFactory.CreateClient("Recipes");
+            recipesList = await client.GetFromJsonAsync<List<Recipe>>("recipes");
+            // Get the required recipe.
         }
     }
 }
