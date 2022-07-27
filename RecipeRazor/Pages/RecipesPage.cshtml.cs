e623449ba16626b5528bd9f66fa360cc4e22451c
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-07-28 00:42:24 +0200

    Added .cshtml.cs file for add recipes page

---
+++ RecipeRazor/Pages/RecipesPage.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeRazor.Models;
+
+namespace RecipeRazor.Pages
+{
+    public class AddRecipesModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AddRecipesModel(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
+ 
+        // Allows us to create recipes from our custom recipe view.
+        [BindProperty]
+        public Recipe recipe { get; set; }
+        public List<string> categ { get; set; } = new List<string>();
+
+        public async Task<IActionResult> OnPost(List<string>data)
+        {
+            var client = _httpClientFactory.CreateClient("Recipes");
+            categ = await client.GetFromJsonAsync<List<string>>("categories");
+            recipe.Ingredients = recipe.Ingredients[0].Split("\r\n").ToList();
+            recipe.Instructions = recipe.Instructions[0].Split("\r\n").ToList();
+            recipe.Categories = data;
+
+            var response = await client.PostAsJsonAsync("recipes/add-recipe", recipe);
+            // Todo: check the response status.
+            return RedirectToPage("./AddRecipes");
+        }
+
+        public async Task OnGet()
+        {
+            var client = _httpClientFactory.CreateClient("Recipes");
+            categ = await client.GetFromJsonAsync<List<string>>("categories");
+
+        }
+    }
+}
