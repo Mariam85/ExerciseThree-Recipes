269ae9acbf68526ea3954c73bbef1cd6e577f62f
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-07-29 03:08:05 +0200

    Modified to handle "add recipes" model.

--- RecipeRazor/Pages/RecipesPage.cshtml.cs
+++ RecipeRazor/Pages/RecipesPage.cshtml.cs
@@ -4,34 +4,30 @@ using RecipeRazor.Models;
 
 namespace RecipeRazor.Pages
 {
-    public class AddRecipesModel : PageModel
+    public class RecipesPageModel : PageModel
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public AddRecipesModel(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
- 
+        public RecipesPageModel(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
+
         // Allows us to create recipes from our custom recipe view.
         [BindProperty]
         public Recipe recipe { get; set; }
         public List<string> categ { get; set; } = new List<string>();
 
-        public async Task<IActionResult> OnPost(List<string>data)
-        {
-            var client = _httpClientFactory.CreateClient("Recipes");
-            categ = await client.GetFromJsonAsync<List<string>>("categories");
-            recipe.Ingredients = recipe.Ingredients[0].Split("\r\n").ToList();
-            recipe.Instructions = recipe.Instructions[0].Split("\r\n").ToList();
-            recipe.Categories = data;
-
-            var response = await client.PostAsJsonAsync("recipes/add-recipe", recipe);
-            // Todo: check the response status.
-            return RedirectToPage("./AddRecipes");
-        }
-
         public async Task OnGet()
         {
             var client = _httpClientFactory.CreateClient("Recipes");
             categ = await client.GetFromJsonAsync<List<string>>("categories");
-
+        }
+        public async Task<IActionResult> OnPost(List<string> data)
+        {
+            var client = _httpClientFactory.CreateClient("Recipes");
+            recipe.Ingredients = recipe.Ingredients[0].Split("\r\n").ToList();
+            recipe.Instructions = recipe.Instructions[0].Split("\r\n").ToList();
+            recipe.Categories = data;
+            var response = await client.PostAsJsonAsync("recipes/add-recipe", recipe);
+            // Todo: check the response status.
+            return Page();
         }
     }
 }
