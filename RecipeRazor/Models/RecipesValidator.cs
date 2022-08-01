ca8a82edac76ae478a21effcbc624332ad67e6d3
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-08-01 03:42:00 +0200

    Added a validator using FluentValidation.

---
+++ RecipeRazor/Models/RecipesValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+namespace WebApplication3.Models
+
+{
+    public class RecipesValidator : AbstractValidator<Recipe>
+    {
+        public RecipesValidator()
+        {
+            RuleFor(recipe => recipe.Ingredients).NotNull().NotEmpty();
+            RuleFor(recipe => recipe.Title).NotNull().NotEmpty();
+            RuleFor(recipe => recipe.Instructions).NotNull().NotEmpty();
+            RuleFor(recipe => recipe.Categories).NotNull().NotEmpty();
+        }
+    }
+}
+
