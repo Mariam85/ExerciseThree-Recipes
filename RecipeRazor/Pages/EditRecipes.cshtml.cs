bf9e75b6f6063f3b82953405044e946a3ec04c8a
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-08-03 04:31:16 +0200

    Handled alerts.

--- RecipeRazor/Pages/EditRecipes.cshtml.cs
+++ RecipeRazor/Pages/EditRecipes.cshtml.cs
@@ -11,19 +11,46 @@ namespace RecipeRazor.Pages
 
         [BindProperty]
         public List<Recipe> recipesList { get; set; } = new List<Recipe>();
-
-        public async Task<IActionResult> OnGet()
+        public List<string> isError { get; set; } = new();
+        public async Task OnGet(List<string> messages)
         {
             var client = _httpClientFactory.CreateClient("Recipes");
             recipesList = await client.GetFromJsonAsync<List<Recipe>>("recipes");
-            return Page();
+            isError = messages;
         }
 
         public async Task<IActionResult> OnPostUpdate(Guid id, string choiceEdit, string afterEdit)
         {
             var client = _httpClientFactory.CreateClient("Recipes");
-            var response = await client.PutAsync($"recipes/edit-recipe/{id}?attributeName={choiceEdit}&editedParameter={afterEdit}", null);
-            return RedirectToPage("./EditRecipes");
+            if (choiceEdit != null && afterEdit != null)
+            {
+                string temp = choiceEdit.Trim();
+                string tmp2 = afterEdit.Trim();
+                if (temp.Length != 0 && tmp2.Length != 0)
+                {
+                    var response = await client.PutAsync($"recipes/edit-recipe/{id}?attributeName={choiceEdit}&editedParameter={afterEdit}", null);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        isError = new List<string> { };
+                        isError.Add("a");
+                        isError.Add("b");
+                    }
+                }
+                else
+                {
+                    isError = new List<string> { };
+                    isError.Add("1");
+                    isError.Add("2");
+                }
+            }
+            else
+            {
+                isError = new List<string> { };
+                isError.Add("1");
+                isError.Add("2");
+            }
+            return RedirectToPage("./EditRecipes", new { messages = isError });
         }
+
     }
 }
